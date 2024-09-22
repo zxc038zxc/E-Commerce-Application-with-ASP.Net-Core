dcb69d48f6437613afdd26cfab1faa957f37604b
DESKTOP-CBGKBMR\User <zxc0.38zxc@gmail.com>
2024-09-22 18:21:38 +0800

    [1. 基礎建置]

---
+++ BookShop/BookShopWeb/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BookShopWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopWeb.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+		{
+			var objCategoryList = _dbContext.Categories.ToList();
+			return View(objCategoryList);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+	}
+}
---
+++ BookShop/BookShopWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShopWeb.Models
+{
+	public class Category
+	{
+        [Key]
+        public int Id { get; set; }
+        [Required] // 避免null
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
